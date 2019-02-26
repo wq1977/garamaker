327a6762a3be9c226cea550456fcc1bed52359fe
wesley <cnwesleywang@gmail.com>
2019-02-26 20:09:05 +0800

    i d better have an osx version

---
+++ lilybuild-osx/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'lilybuild-osx'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
