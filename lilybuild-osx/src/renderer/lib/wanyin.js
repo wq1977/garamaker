c354f737886422feea27f52037fb4b72148b6b6f
wesley <cnwesleywang@gmail.com>
2019-03-11 23:07:47 +0800

    fix wanyin len

--- lilybuild-osx/src/renderer/lib/wanyin.js
+++ lilybuild-osx/src/renderer/lib/wanyin.js
@@ -1,7 +1,14 @@
 const WanUnit = 24
+
+function yinfulen (yinfus, idx) {
+  const yinfu = yinfus.filter(y => y[0] === idx * 2)
+  if (yinfu.length > 0) return yinfu[0][3]
+  return 2
+}
+
 const processunits = {
-  '@p': (yin, idx, yinfus) => [[idx, 1, 0.5 * WanUnit, 0.5 * WanUnit]],
-  '@s': (yin, idx, yinfus) => [[idx, -1, 0, WanUnit]]
+  '@p': (yin, idx, yinfus) => [[idx, 1, 0.5 * WanUnit * yinfulen(yinfus, idx), 0.5 * WanUnit * yinfulen(yinfus, idx)]],
+  '@s': (yin, idx, yinfus) => [[idx, -1, 0, 0.5 * WanUnit * yinfulen(yinfus, idx)]]
 }
 
 export function process (yin, idx, yinfus) {
